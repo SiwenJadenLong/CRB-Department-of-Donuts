f270cb44e3c507ae174eecc87b442915c34cc8ee
Jadenlong <jadenlong376@gmail.com>
2026-01-15 01:06:26 -0800

    New footprints for buck and accel, layout partially done.

--- Donut of Doom/PICO 2 CODE/Pico 2 ELRS Test/src/main.cpp
+++ Donut of Doom/PICO 2 CODE/Pico 2 ELRS Test/src/main.cpp
@@ -6,10 +6,12 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(myFunction(2, 3));
 }
 
 // put function definitions here:
